168dcd4af001a840a89c304c768a51411ac5f95b
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2021-11-15 15:17:13 +0100

    lun 15h15, depart ecole

--- ft_printstr.c
+++ ft_printstr.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 11:09:54 by nchennaf          #+#    #+#             */
-/*   Updated: 2021/11/15 12:53:46 by nchennaf         ###   ########.fr       */
+/*   Updated: 2021/11/15 14:06:19 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,49 @@ char	*ft_printstr(const char *s, ...)
 		str[i] += (char)va_arg(args, int);
 		i++;
 	}
+	va_end(args);
 	str[i] = '\0';
 	return (str);
 }
 /*
 int	main(void)
 {
-	char	*str;
+	char	*str1, *str2, *str3;
+	str1 = "le chien aime la nuit.";
+	str2 = "le chat aussi";
+	str3 = "et sa soeur";
 
-	str = "le chien aime la nuit.";
-	ft_printstr(str, "le chat aussi", "et sa soeur");
+	ft_printstr(str1, str2, str3);
+	return 0;
 }*/
+
+/*FONCTIONNEMENT
+
+On recoit un char* format en parametre et un nombre indefini d'autres parametres
+
+On initialise la structure va_list
+va_list paraminfos et va_start avec la variable va_list (ici paraminfos) et le
+dernier parametre connu (ici format)
+
+On cree et initialise une variable pour le nbr de parametres ecrits
+
+On cree une variable de type char pour le currentchar. On lui assigne la valeur
+de format (en entree) et on l'incremente jusqu'a la fin de la chaine
+On profite de cette boucle pour tester si le caractere actuel n'est pas %. On
+l'affiche a l'aide d'un putchar. On incremente le nbr de caracteres lus.
+
+On se retrouve ici avec un % comme caractere rencontre. On va donc devoir
+decider quoi faire avec selon le caractere suivant. If else, elseif infini qui
+appellerai les fonctions adequates (dans les autres fichiers).
+
+Dans le cas d'une chaine de caracteres :
+On cree une constante de type char*. On lui assigne le va_arg caste en constante
+de char* a l'aide de la variable va_list suivie du type const char*.
+On va ensuite boucler tant que la constante n'est pas terminee, en affichant
+a l'aide d'un putchar les index suivants et en augmentant le nbr de caracteres
+lus.
+
+A la fin, on appelle la macro va_end avec la variable va_list.
+On retourne ensuite la variable caracteres lus.
+
+*/
